fb2c77dcdf27bb5aca1e7afe8473481afe024f3c
naichenzhao <naichenzhao13@gmail.com>
2024-01-09 23:33:46 -0800

    ordf with joint limits

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/naichenzhao/Desktop/SLICE-Spider/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/naichenzhao/ws_moveit/devel;/opt/ros/noetic'.split(';') if '/home/naichenzhao/ws_moveit/devel;/opt/ros/noetic' != '' else []
---
+++ catkin_ws/build/spider-viz/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/naichenzhao/Desktop/SLICE-Spider/catkin_ws/src/spider-viz
+# Build directory: /home/naichenzhao/Desktop/SLICE-Spider/catkin_ws/build/spider-viz
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
